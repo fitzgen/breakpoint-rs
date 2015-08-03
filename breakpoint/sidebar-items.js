6adda2fb33ac8b51626b71a430319017b85941c2
Nick Fitzgerald <fitzgen@gmail.com>
2015-08-03 09:56:19 -0700

    Update docs to: cf65d72 Add README, expand Cargo.toml manifest metadata

---
+++ breakpoint/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"macro":[["breakpoint!","Set a breakpoint."]]});
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['breakpoint'] = {"items":[[0,"","breakpoint","Set breakpoints from within the comfort of your editor.",null,null],[14,"breakpoint!","","Set a breakpoint.",null,null]],"paths":[]};
+initSearch(searchIndex);
